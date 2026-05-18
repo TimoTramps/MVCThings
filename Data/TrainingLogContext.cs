c2a62d5b57ac73c0fc588fc5541c576e8f0c2f9d
Timotej Trampuz <timotej.trampuz@tscng.org>
2026-05-18 17:14:36 +0200

    Add files via upload

---
+++ Data/TrainingLogContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingLog.Models;
+
+namespace TrainingLog.Data
+{
+    public class TrainingLogContext : DbContext
+    {
+        public TrainingLogContext(DbContextOptions<TrainingLogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Athlete> Athlete { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+    }
+}
